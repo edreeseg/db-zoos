5e5b0d9b180779e625172bfdd5a5c6b533407e3f
edreeseg <edward.reeseg@gmail.com>
2019-02-25 13:03:34 -0500

    CRUD operations for both base and stretch complete

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ server.post('/api/zoos', (req, res) => {
   if (!name) {
     return res
       .status(400)
-      .json({ error: 'Request must include a name key in the body.' });
+      .json({ error: 'Request body must include a name key.' });
   }
   db.insert({ name })
     .into('zoos')
@@ -52,6 +52,120 @@ server.get('/api/zoos/:id', (req, res) => {
       req
         .status(500)
         .json({ error: 'There was an error while attempting to get zoo.' })
+    );
+});
+
+server.delete('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  db('zoos')
+    .where({ id }) // See above
+    .del()
+    .then(deleted =>
+      deleted
+        ? res.json({ deleted })
+        : res.status(404).json({ error: 'No zoo with that ID found.' })
+    )
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: 'There was an error while attempting to delete entry.' })
+    );
+});
+
+server.put('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ error: 'Request body must include a name key.' });
+  }
+  db('zoos')
+    .where({ id }) // See above
+    .update({ name })
+    .then(updated =>
+      updated
+        ? res.json({ updated })
+        : res.status(404).json({ error: 'No zoo found with that id.' })
+    )
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: 'There was an error while attempting to update entry.' })
+    );
+});
+
+server.post('/api/bears', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ error: 'Request body must include a name key.' });
+  }
+  db.insert({ name })
+    .into('bears')
+    .then(id => res.status(201).json({ id: id[0] }))
+    .catch(err => res.status(500).json({ error: err }));
+});
+
+server.get('/api/bears', (req, res) => {
+  db('bears')
+    .then(bears => res.json({ bears }))
+    .catch(err => res.status(500).json({ err }));
+});
+
+server.get('/api/bears/:id', (req, res) => {
+  const { id } = req.params;
+  db('bears')
+    .where({ id })
+    .then(bear =>
+      bear.length
+        ? res.json({ bear: bear[0] })
+        : res.status(404).json({ error: 'No bear found with that id.' })
+    )
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: 'There was an error while trying to get entry.' })
+    );
+});
+
+server.delete('/api/bears/:id', (req, res) => {
+  const { id } = req.params;
+  db('bears')
+    .where({ id })
+    .del()
+    .then(deleted =>
+      deleted
+        ? res.json({ deleted })
+        : res.status(404).json({ error: 'No bear found with that id.' })
+    )
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: 'There was an error while trying to delete entry.' })
+    );
+});
+
+server.put('/api/bears/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  if (!name)
+    return res
+      .status(400)
+      .json({ error: 'Request body must include a name key.' });
+  db('bears')
+    .where({ id })
+    .update({ name })
+    .then(updated =>
+      updated
+        ? res.json({ updated })
+        : res.status(404).json({ error: 'No bear found with that id.' })
+    )
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: 'There was an error while trying to update entry.' })
     );
 });
 
